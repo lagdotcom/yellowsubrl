913413a09243aef9e6df5134ba8e29f45c4036af
Paul Davies <lagdotcom@gmail.com>
2020-06-23 18:44:54 +0100

    replaced `FovAlgorithm.Raycasting` with bbox/bline-based raycasting algorithm, renamed mine to `FovAlgorithm.Sunburst` (it does seem more accurate but slower)

--- src/libtcod/Map.ts
+++ src/libtcod/Map.ts
@@ -1,9 +1,65 @@
 export enum FovAlgorithm {
-	Raycasting = 0,
+	Raycasting,
+	Sunburst,
 }
 
-const raycastRays = 360;
-function raycast(
+function raycasting(
+	map: Map,
+	cx: number,
+	cy: number,
+	radius: number,
+	lightWalls: boolean
+) {
+	const minX = Math.max(0, cx - radius);
+	const maxX = Math.min(map.width - 1, cx + radius);
+	const minY = Math.max(0, cy - radius);
+	const maxY = Math.min(map.height - 1, cy + radius);
+	const rad2 = radius * radius;
+
+	function ray(x1: number, y1: number) {
+		var x0 = cx,
+			y0 = cy;
+		const dx = Math.abs(x1 - x0);
+		const sx = x0 < x1 ? 1 : -1;
+		const dy = -Math.abs(y1 - y0);
+		const sy = y0 < y1 ? 1 : -1;
+		var err = dx + dy;
+		while (true) {
+			const distX = Math.abs(x0 - cx);
+			const distY = Math.abs(y0 - cy);
+			if (distX * distX + distY * distY > rad2) return;
+
+			if (!map.contains(x0, y0)) return;
+			const t = map.tiles[x0][y0];
+			t.inFov = lightWalls || t.allowSight;
+			if (!t.allowSight) return;
+
+			if (x0 == x1 && y0 == y1) return;
+
+			const e2 = 2 * err;
+			if (e2 >= dy) {
+				err += dy;
+				x0 += sx;
+			}
+			if (e2 <= dx) {
+				err += dx;
+				y0 += sy;
+			}
+		}
+	}
+
+	for (var x = minX; x <= maxX; x++) {
+		ray(x, minY);
+		ray(x, maxY);
+	}
+	for (var y = minY; y <= maxY; y++) {
+		ray(minX, y);
+		ray(maxX, y);
+	}
+}
+
+const sunRays = 90;
+function sunburst(
 	map: Map,
 	cx: number,
 	cy: number,
@@ -30,12 +86,13 @@ function raycast(
 		}
 	};
 
-	const increment = (Math.PI * 2) / raycastRays;
+	const increment = (Math.PI * 2) / sunRays;
 	for (var a = 0; a < Math.PI * 2; a += increment) ray(a);
 }
 
 const algorithms = {
-	[FovAlgorithm.Raycasting]: raycast,
+	[FovAlgorithm.Raycasting]: raycasting,
+	[FovAlgorithm.Sunburst]: sunburst,
 };
 
 class MapTile {
